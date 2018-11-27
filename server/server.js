3ac5f996e79d4e5dcf7d7d20237999c7a1ef16da
Igor Iliev <i.iliev@euroslot.biz>
2018-11-27 19:01:37 +0100

    dodadeni connect i disconnect

--- server/server.js
+++ server/server.js
@@ -1,16 +1,25 @@
 const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO=require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-
-const express = require('express');
-
-var app = express();
 const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) =>{
+  console.log('nov korisnik');
 
+  socket.on('disconnect', () => {
+    console.log('korisnikot se otkaci');
+  });
 
-app.listen(port, () => {
+});
+
+server.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
