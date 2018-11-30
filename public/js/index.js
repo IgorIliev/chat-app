322d0f72287d2ada7351382436edd6940b6ad5d7
Igor Iliev <i.iliev@euroslot.biz>
2018-11-30 17:09:55 +0100

    formated timestamps using moment.js

--- public/js/index.js
+++ public/js/index.js
@@ -9,10 +9,9 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  console.log(`new mesage from: ${message.from}
-    createdAt:${message.createdAt} : ${message.text}`);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
     jQuery('#messages').append(li);
 });
@@ -20,8 +19,8 @@ socket.on('newMessage', function (message) {
 socket.on('newLocationMessage', function (message) {
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My current location</a>');
-
-  li.text(`${message.from}: `);
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
